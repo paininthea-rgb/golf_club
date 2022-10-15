e75a19ab8faafae67587b2c531ae67b42d97b517
unknown <c_hoang2@fanshaweonline.ca>
2022-10-14 22:34:05 -0400

    final code

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+	"use strict";
+	console.log("fired");
+
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu(){
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+})();
